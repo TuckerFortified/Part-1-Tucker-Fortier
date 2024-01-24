2a9c2d3128e31dcf8185f1893e25d9fddec6f10c
Tucker Fortier <tucker.fortier@gmail.com>
2024-01-24 17:36:43 -0500

    Finished character movement. On to the next task.

---
+++ Assets/Assignment/Assignment 1/Scripts/Character_Movement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Movement : MonoBehaviour
+{
+
+    Vector2 direction;
+    public Rigidbody2D rigidbody;
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.AddForce(force * direction * Time.deltaTime);
+    }
+}
