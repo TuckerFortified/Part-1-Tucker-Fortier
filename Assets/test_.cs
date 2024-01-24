0510a54a9c65cdca655dda2e881ff5347ba2d707
Tucker Fortier <tucker.fortier@gmail.com>
2024-01-24 18:54:22 -0500

    Finished setting up the prefab, spawner, and rectangle trigger. There was some sort of error where I had to create the c# script inside the component manager (for the prefab). The script is called "test_" and I have no idea where it is located, but it works!

--- Assets/test_.cs
+++ Assets/test_.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.VFX;
 
-public class Missile : MonoBehaviour
+public class test_ : MonoBehaviour
 {
     Rigidbody2D rigidbody;
     public float speed;
@@ -16,18 +15,12 @@ public class Missile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.Translate(1 * speed * Time.deltaTime, 0, 0);
+        
     }
 
     private void FixedUpdate()
     {
         Vector2 direction = new Vector2(speed * Time.deltaTime, 0);
         rigidbody.MovePosition(rigidbody.position + direction);
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("missile collision");
-        Destroy(gameObject);
     }
 }
